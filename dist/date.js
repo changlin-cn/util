0b46bea4b1be2e2c9c51b98fac07419317181536
changlin <ChangLin-CN@users.noreply.github.com>
2017-12-25 10:45:31 +0800

    update regex

--- dist/date.js
+++ dist/date.js
@@ -84,7 +84,7 @@ function dateFormat(date, format) {
     };
     if (/(y+)/.test(format)) format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
     for (var k in o) {
-        if (new RegExp("(" + k + ")").test(format)) format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+        if (new RegExp("(" + k + ")").test(format)) format = format.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
     }return format;
 }
 
